9e8922f7674906f518efa94f97f03962027d9b5e
Eduardo Sandoval <ceoblackcatvk@gmail.com>
2025-05-03 23:24:06 -0600

    desde0

--- src/App.js
+++ src/App.js
@@ -17,21 +17,6 @@ function App() {
         >
           Learn React
         </a>
-
-        <IDKitWidget
-        app_id="app_9107e5e1f88c0e8e568869ccb2fa3fed"
-        action="worldapp_login"
-        signal="ingresoapp"
-        handleVerify={handleVerify}
-        onSuccess={handleSuccess}
-        onError={(err) => {
-          console.error("❌ Error en World ID:", err);
-          alert("Error con la verificación");
-        }}
-      >
-        {({ open }) => <button onClick={open}>Verificar con World ID</button>}
-      </IDKitWidget>
-
       </header>
     </div>
   );

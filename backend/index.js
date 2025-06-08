ae6712812b1d32f16b50efa3cf4fe0f7540168ed
Eduardo Sandoval <ceoblackcatvk@gmail.com>
2025-06-08 02:50:04 -0600

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://worldapp-miniapp.vercel.app' // ← tu frontend
+}));
 app.use(express.json());
 
 let tasks = [];
